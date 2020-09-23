973b51edb1525a1163b372219fd007ded9826c29
v-ndy <n.andy@MAC-BOOK-PRO-de-NEGRIT>
2020-09-23 11:35:35 +0200

    feat(Exercice1): create a variable

---
+++ variable/index.js
@@ -0,0 +1,22 @@
+'use strict';
+/**
+ * Exercice 1
+ */
+ // My digital School Forever
+ 
+ /*
+   My Digital School forever every day
+ */
+
+ var string = 42 ;
+ var number = 42 ;
+ var float = 42;
+ var boolean = ;
+ var array = 42;
+ var date  = 42;
+ var objet = 42 ;
+ var undefined = ;
+ var null =;
+ var my42count= 'quarante_deux'{
+     char = 12
+ }
