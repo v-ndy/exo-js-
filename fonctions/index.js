ad4df7885c1bda536f25602922abe1e7ebe5c4ad
v-ndy <n.andy@MAC-BOOK-PRO-de-NEGRIT>
2020-09-24 22:18:06 +0200

    feat(fonctions): create fuction

---
+++ fonctions/index.js
@@ -0,0 +1,27 @@
+'use strict';
+
+/**
+ * Exercice 1
+ */
+
+/**
+ * Returns the sum of string 
+ * @param {arg} string
+ * @param {arg} number
+ * @param {alert} si number different string so poster message 
+ * @returns {string} no printed alert.
+ */
+
+var myPutStr = function myPutStr(string, number) {
+
+    if (number > string) {
+        alert("et pourquoi pas 42 ?")
+}
+
+    return string;
+};
+
+var myCalculator = function computeSurfaceM2 (L,l){
+   
+    return  L * l * 3;
+};
